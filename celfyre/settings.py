ec4a3cdf4f209c63363cb7cd08c28b1f01767358
Manuel Zorzi <manuel.zorzi@filament.ai>
2023-05-27 18:06:38 +0200

    update dramatiq.

--- celfyre/settings.py
+++ celfyre/settings.py
@@ -136,7 +136,6 @@ django_heroku.settings(locals())
 
 import dramatiq
 from dramatiq.brokers.redis import RedisBroker
-import os
 
 redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 broker = RedisBroker.from_url(redis_url)
